048758dd4c6e9637135ae9cfe7396e3da4711bf5
Rafael Martins <rafaelmartinsch@gmail.com>
2025-01-10 19:08:46 -0400

    Credenciais do banco de dados no .env

--- database/conector.py
+++ database/conector.py
@@ -1,13 +1,13 @@
 import mysql.connector
-# import os
-# from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv
 
 
-# load_dotenv()
-db_user = 'root' #os.getenv("DB_USER")
-db_password = '12345678'#os.getenv("DB_PASSWORD")
-db_host = 'localhost' #os.getenv("DB_HOST")
-db_name = 'localhost' #os.getenv("DB_NAME")
+load_dotenv()
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
 
 
 
